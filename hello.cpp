16166fdfcd8c58b8588a1ff021df8cf09b47e262
Coder <surajprakash1949@gmail.com>
2023-07-17 19:19:06 +0530

    Prime numbers

--- hello.cpp
+++ hello.cpp
@@ -9,8 +9,10 @@ int main(){
   while(i<n){
     if (n%i==0)
     {
-      cout<<"Not prime"<<endl;
-      break;
+      cout<<"Not prime for "<<i<<endl;
+    }
+    else{
+      cout<<"Prime for "<< i <<endl;
     }
     i=i+1;
     
