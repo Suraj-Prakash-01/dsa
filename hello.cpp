31d70ab8ba9521a6af9a985658ac7d928c3927ae
Coder <surajprakash1949@gmail.com>
2023-07-18 21:18:43 +0530

    Pattern 7

--- hello.cpp
+++ hello.cpp
@@ -5,15 +5,17 @@ using namespace std;
 int main(){
    int n;
    cin>>n;
-   int row=1;
-   while(row<=n){
-    int col=1;
-    while(col<=row){
-      cout<<row+col-1;
-      col=col+1;
+   int i=1;
+   while(i<=n){
+    int j=1;
+    while(j<=i){
+      cout<<i-j+1;
+      j+=1;
     }
     cout<<endl;
-    row=row+1;
-  }
+    i=i+1;
+
+   }
 }
+
  
