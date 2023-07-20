73d3829e7ecb1d1c5f689a9af4e8c2e76f5fa11d
Coder <surajprakash1949@gmail.com>
2023-07-20 18:59:59 +0530

    Checking Prime

--- hello.cpp
+++ hello.cpp
@@ -7,15 +7,21 @@ int main(){
   cout<<" Enter the value of n"<<endl;
   cin>>n;
 
-  int a=0;
-  int b=1;
-  cout<<a<<" "<<endl;
-  cout<<b<<" "<<endl;
-  for(int i=1; i<=n;i++){
-    int nxt=a+b;
-    cout<<nxt<<endl;
-    a=b;
-    b=nxt;
+  bool isPrime=1;
 
+  for(int i=2;i<n;i++){
+    if(n%i==0){
+      
+      isPrime=0;
+      break;
+    }
+    
+  }
+  if(isPrime==0){
+    cout<<" Not a prime Number"<<endl;
+  }
+  else{
+    cout<<" Prime number"<<endl;
   }
 }
+
