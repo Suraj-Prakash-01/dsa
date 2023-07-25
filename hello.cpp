050c192e488c3334b1f0d85e9c9d32087f9c2503
Coder <surajprakash1949@gmail.com>
2023-07-25 19:34:07 +0530

    Simple Calculator

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-  int p=1;
-  int sum=0;
+int main()
+{
+  int a, b;
+  cout << "Enter the value of a " << endl;
+  cin >> a;
 
-  while(n!=0){
-    int rem=n%10;
-    p*=rem;
-    sum=sum+rem;
-    n=n/10;
+  cout << "Enter the value of b " << endl;
+  cin >> b;
+
+  char op;
+  cout << "Enter the operation you want to perform " << endl;
+  cin >> op;
+
+  switch (op)
+  {
+  case '+':
+    cout << (a + b) << endl;
+    break;
+  case '-':
+    cout << (a - b) << endl;
+    break;
+  case '*':
+    cout << (a * b) << endl;
+    break;
+  case '/':
+    cout << (a / b) << endl;
+    break;
+  case '%':
+    cout << (a % b) << endl;
+    break;
+  default:
+    cout << "Please enter a valid operation" << endl;
   }
-  int answer=p-sum;
-  return answer;
 }
-
