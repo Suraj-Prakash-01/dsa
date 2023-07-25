f506a554f925f69486eff8b71b21e43ea52bd37e
Coder <surajprakash1949@gmail.com>
2023-07-25 21:48:52 +0530

    Power function

--- hello.cpp
+++ hello.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int power(int a, int b)
+{
+  int ans = 1;
+  for (int i = 1; i <= b; i++)
+  {
+    ans = ans * a;
+  }
+  return ans;
+}
+
 int main()
 {
   int a, b;
-  cout << "Enter the value of a " << endl;
-  cin >> a;
-
-  cout << "Enter the value of b " << endl;
-  cin >> b;
-
-  char op;
-  cout << "Enter the operation you want to perform " << endl;
-  cin >> op;
-
-  switch (op)
-  {
-  case '+':
-    cout << (a + b) << endl;
-    break;
-  case '-':
-    cout << (a - b) << endl;
-    break;
-  case '*':
-    cout << (a * b) << endl;
-    break;
-  case '/':
-    cout << (a / b) << endl;
-    break;
-  case '%':
-    cout << (a % b) << endl;
-    break;
-  default:
-    cout << "Please enter a valid operation" << endl;
-  }
+  cin >> a >> b;
+  int ans = power(a, b);
+  cout << "Answer is " << ans << endl;
+  return 0;
 }
