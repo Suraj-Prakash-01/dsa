4f98dd35f03f0c8ab15b4a6ecda55dc59fddd50c
Coder <surajprakash1949@gmail.com>
2023-09-10 09:35:45 +0530

    AP function

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 
-bool isPrime(int n)
+int AP(int n)
 {
-  for (int i = 2; i < n; i++)
-  {
-    if (n % i == 0)
-    {
-      return 0;
-    }
-  }
-  return 1;
+  return (3 * n + 7);
 }
 
 int main()
@@ -17,12 +10,5 @@ int main()
   int n;
   std::cin >> n;
 
-  if (isPrime(n))
-  {
-    std::cout << n << " is a prime number";
-  }
-  else
-  {
-    std::cout << n << " is not a prime number";
-  }
+  std::cout << "The n'th term of the AP is " << AP(n) << std::endl;
 }
