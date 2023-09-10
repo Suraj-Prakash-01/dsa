5d50b1ba4b57f5f506da21c8bd7765eaf97ce16e
Coder <surajprakash1949@gmail.com>
2023-09-10 14:43:01 +0530

    Basic Linear Search

--- hello.cpp
+++ hello.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
-#include <limits.h>
-#include <algorithm>
+
+bool search(int arr[], int size, int key)
+{
+  for (int i = 0; i < size; i++)
+  {
+    if (arr[i] == key)
+    {
+      return 1;
+    }
+  }
+  return 0;
+}
 
 int main()
 {
-  int arr[5];
-  int mini = INT_MAX;
-  int size = sizeof(arr) / sizeof(int);
+  int arr[10] = {5, 7, -2, 10, 22, -2, 0, 5, 22, 1};
+  int key;
+  std::cin >> key;
 
-  std::cout << "size of array is " << size << std::endl;
-  for (int i = 0; i < size; i++)
-  {
-    std::cin >> arr[i];
-  }
+  bool found = search(arr, 10, key);
 
-  for (int j = 0; j < size; j++)
+  if (found)
   {
-    mini = std::min(mini, arr[j]);
+    std::cout << "Key is present" << std::endl;
   }
-  std::cout << "The minimum value is " << mini;
+  else
+  {
+    std::cout << "Key not found" << std::endl;
+  }
 }
