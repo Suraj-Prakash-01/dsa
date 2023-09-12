d452a031aad09cafa1460d650f46f072f5201a3f
Coder <surajprakash1949@gmail.com>
2023-09-12 09:12:08 +0530

    Setprecision Funtion

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
+#include <iomanip> // contains setprecision function
 
 int main()
 {
-  int ans = 0;
-  int size;
-  std::cin >> size;
-  int arr[7] = {1, 2, 3, 3, 4, 5, 1};
-
-  for (int i = 0; i < size; i++)
-  {
-    ans = ans ^ arr[i]; // Xor makes same entries zero
-  }
-  std::cout << ans;
+  double f = 15.784315289;
+  // Fixed brings the cursor to the decimal
+  // so that number can be counted after that
+  std::cout << std::fixed;
+  std::cout << std::setprecision(5) << f << std::endl;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,1 @@
-#include <iostream>
-#include <cmath>
-
-int main()
-{
-  int n;
-  std::cin >> n;
-  int bit;
-  int ans = 0;
-  int i = 0;
-  while (n != 0)
-  {
-    bit = n & 1;
-    n = n >> 1;
-    ans = (bit * pow(10, i)) + ans;
-    i++;
-  }
-  std::cout << ans;
 }
