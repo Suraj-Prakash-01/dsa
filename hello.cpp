4e18245ea5a250e3e78f776a161f9a79c55c498a
Coder <surajprakash1949@gmail.com>
2023-09-12 09:36:43 +0530

    Swapping using XOR

--- hello.cpp
+++ hello.cpp
@@ -7,9 +7,9 @@ int main()
 
   std::cout << "The value before swapping is : " << a << " " << b << std::endl;
 
-  a = a + b;
-  b = a - b;
-  a = a - b;
+  a = a ^ b;
+  b = a ^ b;
+  a = a ^ b;
 
   std::cout << "The value after swapping : " << a << " " << b << std::endl;
 }
