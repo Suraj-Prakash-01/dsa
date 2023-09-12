9a320aeb869c692f4e027fe461456457151ba7ab
Coder <surajprakash1949@gmail.com>
2023-09-12 09:30:08 +0530

    Swapping numbers without third variable

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include <iomanip> // contains setprecision function
 
 int main()
 {
-  double f = 15.784315289;
-  // Fixed brings the cursor to the decimal
-  // so that number can be counted after that
-  std::cout << std::fixed;
-  std::cout << std::setprecision(5) << f << std::endl;
+  int a, b;
+  std::cin >> a >> b;
+
+  std::cout << "The value before swapping is : " << a << " " << b << std::endl;
+
+  a = a + b;
+  b = a - b;
+  a = a - b;
+
+  std::cout << "The value after swapping : " << a << " " << b << std::endl;
 }
