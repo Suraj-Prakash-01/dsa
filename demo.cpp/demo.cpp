b1dab8a380c4a79b638f93e667064d881c169554
Coder <surajprakash1949@gmail.com>
2023-09-14 09:49:27 +0530

    Sorting array

--- demo.cpp/demo.cpp
+++ demo.cpp/demo.cpp
@@ -1,48 +1,13 @@
 #include <iostream>
-
-void printArray(int arr[], int n)
-{
-  for (int i = 0; i < n; i++)
-  {
-    std::cout << arr[i] << " ";
-  }
-  std::cout << std::endl;
-}
-
-void sortOne(int arr[], int n)
-{
-  int left = 0, right = n - 1;
-
-  while (left < right)
-  {
-
-    while (arr[left] == 0 && left < right)
-    {
-      left++;
-    }
-
-    while (arr[right] == 1 && left < right)
-    {
-      right--;
-    }
-
-    // Since we are here, it means
-    // arr[left]==1 amd arr[right]==0
-    if (left < right)
-    {
-      std::swap(arr[left], arr[right]);
-      left++;
-      right--;
-    }
-  }
-}
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-  int arr[8] = {1, 1, 0, 0, 0, 0, 1, 0};
+  std::vector<int> arr1 = {1, 2, 4, 2, 8};
+  std::vector<int> arr2 = {2, 5, 3, 8, 6, 9, 1, 3, 6};
 
-  sortOne(arr, 8);
-  printArray(arr, 8);
-
-  return 0;
+  std::sort(arr2.begin(), arr2.end());
+  for (int i = 0; i < arr2.size(); i++)
+    std::cout << " " << arr2[i] << std::endl;
 }
