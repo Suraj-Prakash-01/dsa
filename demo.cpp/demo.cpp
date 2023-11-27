716cb5d8b8c7cfb4bb7ea87487c287180a8c2eb5
Coder <surajprakash1949@gmail.com>
2023-11-27 11:16:16 +0530

    Insertion sort

--- demo.cpp/demo.cpp
+++ demo.cpp/demo.cpp
@@ -2,25 +2,37 @@
 using std::cin;
 using std::cout;
 
-void bubble_sort(int arr[], int n)
+void insertion_sort(int arr[], int n)
 {
+  // We started with i=1 bcz we considered
+  // First element as already sorted one
   for (int i = 1; i < n; i++)
   {
-    // For round 1 ot n-1
-
-    for (int j = 0; j < n - i; j++)
+    int temp = arr[i];
+    int j = i - 1;
+    for (; j >= 0; j--)
     {
-      if (arr[j] > arr[j + 1])
-        std::swap(arr[j], arr[j + 1]);
+      if (arr[j] > temp)
+      {
+        // Shift in position to make space for new element
+        arr[j + 1] = arr[j];
+      }
+      else
+      {
+        // In order to stop if the element doesn't need to be sorted
+        break;
+      }
     }
+    // To input value at the space created
+    arr[j + 1] = temp;
   }
 }
 
 int main()
 {
-  int arr[] = {5, 2, 8, 1, 9};
+  int arr[] = {5, 2, 11, 9, 1};
   int n = sizeof(arr) / sizeof(arr[0]);
-  bubble_sort(arr, n);
+  insertion_sort(arr, n);
   cout << "The sorted array is ";
   for (int i = 0; i < n; i++)
     cout << arr[i] << " ";
