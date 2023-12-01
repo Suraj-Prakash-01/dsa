d472d49b1cf7bbe83d0f7508c81a4fc400cd7641
Coder <surajprakash1949@gmail.com>
2023-12-01 19:28:32 +0530

    2-D Array initialisation

--- demo.cpp/jpmcq2.cpp
+++ demo.cpp/jpmcq2.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using std::cin;
 using std::cout;
+using std::string;
 
 int duplicate(int arr[], int n)
 {
@@ -35,4 +38,35 @@ int main()
 
   cout << "The number of unique elements in sorted array are " << duplicate(arr, n) << std::endl;
   print(arr, n);
+}
+
+int compress(std::vector<char> &chars)
+{
+  int i = 0;
+  int ansIndex = 0;
+  int n = chars.size();
+
+  while (i < n)
+  {
+    // Initialising j so that we can compare it with i index
+    int j = i + 1;
+    while (j <= n && chars[i] == chars[j])
+      j++;
+
+    // To store old character
+    chars[ansIndex++] = chars[i];
+
+    int count = j - i;
+
+    if (count > 1)
+    {
+      // To convert counting into single digit and saving in answer
+      string cnt = std::to_string(count);
+      for (char ch : cnt)
+        chars[ansIndex++] = ch;
+    }
+    // redirecting the pointer to j
+    i = j;
+  }
+  return ansIndex;
 }
--- practice.cpp
+++ practice.cpp
@@ -1,52 +1,28 @@
 #include <iostream>
-#include <string>
 using std::cin;
 using std::cout;
 
-char getMaxOccChar(std::string s)
-{
-  int arr[26] = {0};
-
-  // Creating an array of count of characters
-  for (int i = 0; i < s.length(); i++)
-  {
-    char ch = s[i];
-
-    // If character is lowercase
-    if (islower(ch))
-    {
-      int num = ch - 'a';
-      arr[num]++;
-    }
-
-    // If character is uppercase
-    else if (isupper(ch))
-    {
-      int num = ch - 'A';
-      arr[num]++;
-    }
-  }
-
-  // Finding max occurrence character
-  int maxi = -1, ans = 0;
-  for (int i = 0; i < 26; i++)
-  {
-    if (maxi < arr[i])
-    {
-      ans = i;
-      maxi = arr[i];
-    }
-  }
-
-  char finalAns = 'a' + ans;
-  return finalAns;
-}
-
 int main()
 {
-  std::string s;
-  cout << "Enter a string :" << std::endl;
-  cin >> s;
+  int arr[3][4];
 
-  cout << "Maximum occuring character is :" << getMaxOccChar(s) << std::endl;
+  cout << "Enter the elements :" << std::endl;
+  // For taking input in 2-D array
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      cin >> arr[i][j];
+    }
+  }
+
+  // For printing 2-D array
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      cout << arr[i][j] << " ";
+    }
+    cout << std::endl;
+  }
 }
