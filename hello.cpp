6f09cea7f47fb49acd271a06478fbcf938e6bd80
Coder <surajprakash1949@gmail.com>
2023-12-02 20:44:52 +0530

    Wave Print

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,19 @@
-#include <stdio.h>
-using namespace std;
+#include <iostream>
 
 int main()
 {
-
-  cout << "Suraj" << endl;
+  int n;
+  std::cin >> n;
+  int i = 1;
+  while (i <= n)
+  {
+    int j = 1;
+    while (j <= n)
+    {
+      std::cout << 3 * (i - 1) + j;
+      j = j + 1;
+    }
+    std::cout << std::endl;
+    i += 1;
+  }
 }
