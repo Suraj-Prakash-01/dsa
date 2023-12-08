428acc92b83000fee96ad71e7fc9eea75a20c56e
Coder <surajprakash1949@gmail.com>
2023-12-08 19:34:08 +0530

    GCD

--- Lecture_24/GCD.cpp
+++ Lecture_24/GCD.cpp
@@ -4,9 +4,21 @@ using std::cout;
 
 int gcd(int a, int b)
 {
+  if (a == 0)
+    return b;
+
   if (b == 0)
     return a;
-  return
+
+  while (a != b)
+  {
+    if (a > b)
+      a = a - b; // Uses Euler's theorem
+
+    else
+      b = b - a; // Uses Euler's theorem
+  }
+  return a;
 }
 
 int main()
@@ -15,5 +27,5 @@ int main()
   cout << "Enter the values of a and b " << std::endl;
   cin >> a >> b;
 
-  cout << "The GCD is" << gcd(a, b) << std::endl;
+  cout << "The GCD is " << gcd(a, b) << std::endl;
 }
