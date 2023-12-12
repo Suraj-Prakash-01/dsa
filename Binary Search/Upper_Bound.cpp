d4b22b0e48d58342a890f45af80de67d138033a5
Coder <surajprakash1949@gmail.com>
2023-12-12 20:19:25 +0530

    Implementation of Upper Bound

---
+++ Binary Search/Upper_Bound.cpp
@@ -0,0 +1,24 @@
+int upperBound(vector<int> &arr, int x, int n)
+{
+  int low = 0, high = n - 1;
+  int ans = n;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+
+    // Maybe an answer
+    if (arr[mid] > x)
+    {
+      ans = mid;
+
+      // Looking for more small index on left
+      high = mid - 1;
+    }
+
+    else
+    {
+      low = mid + 1; // Look for right
+    }
+  }
+  return ans;
+}
