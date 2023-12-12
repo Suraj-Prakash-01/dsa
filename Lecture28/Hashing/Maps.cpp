a18c1826c69833116c1ee26ceebd9096f9628e14
Coder <surajprakash1949@gmail.com>
2023-12-12 07:17:11 +0530

    Basic Maps

---
+++ Lecture28/Hashing/Maps.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+using std::cin;
+using std::cout;
+
+int main()
+{
+  int arr[10];
+  for (int i = 0; i < 10; i++)
+  {
+    cin >> arr[i];
+  }
+
+  // Pre-Compute
+  // <int,int> stores <number,frequency>
+  std::map<int, int> mpp;
+  for (int i = 0; i < 10; i++)
+    mpp[arr[i]]++;
+
+  int q;
+  cout << "Enter number of queries" << std::endl;
+  cin >> q;
+  while (q--)
+  {
+    int number;
+    cin >> number;
+    // Fetchinh numbrt gives frequency
+    cout << mpp[number] << std::endl;
+  }
+  return 0;
+}
