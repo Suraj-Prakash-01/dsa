435631b6d5076caf2784f4a1da75c999c601d53f
Coder <surajprakash1949@gmail.com>
2023-12-14 10:24:32 +0530

    Finding Peak Element

---
+++ Binary Search/Peak_element.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+  int findPeakElement(vector<int> &nums)
+  {
+    int n = nums.size();
+
+    // Considering edge cases
+    if (n == 1)
+      return 0;
+    if (nums[0] > nums[1])
+      return 0;
+    if (nums[n - 1] > nums[n - 2])
+      return n - 1;
+
+    int low = 1, high = n - 2;
+    while (low <= high)
+    {
+      int mid = (low + high) / 2;
+      // Checking whether mid is peak value or not
+      if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+        return mid;
+      // Trimming down the search space by removing increasing value region
+      else if (nums[mid] > nums[mid - 1])
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+    return -1;
+  }
+};
