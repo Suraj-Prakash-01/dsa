8618a9860c23ce56a4692537c8160791a09acc8d
Coder <surajprakash1949@gmail.com>
2024-01-06 21:05:28 +0530

    Richest Customer Wealth

---
+++ Leetcode/Richest_Customer_Wealth.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+  int maximumWealth(vector<vector<int>> &accounts)
+  {
+    int sum = 0;
+    int maxi = INT_MIN;
+    for (int i = 0; i < accounts.size(); i++)
+    {
+      for (int j = 0; j < accounts[0].size(); j++)
+      {
+        sum += accounts[i][j];
+      }
+      maxi = max(maxi, sum);
+      sum = 0;
+    }
+    return maxi;
+  }
+};
