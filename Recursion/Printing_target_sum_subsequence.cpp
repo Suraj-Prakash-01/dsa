aef8d1df2e1ffd33452389208754df319d768b4d
Coder <surajprakash1949@gmail.com>
2024-01-18 09:35:44 +0530

    Printing subsequence with target sum

---
+++ Recursion/Printing_target_sum_subsequence.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using std::cin;
+using std::cout;
+
+void printS(int i, std::vector<int> &ans, int s, int sum, int arr[], int n)
+{
+  if (i == n) // Here i is index
+  {
+    if (s == sum) // Print only if the sum of subsequence is target sum
+    {
+      for (auto it : ans)
+      {
+        cout << it << " ";
+      }
+      cout << std::endl;
+    }
+    // Return the answer after every potential sum it finds
+    return;
+  }
+
+  // From here, it's usual structure
+  ans.push_back(arr[i]);
+  s += arr[i];
+  // Take or Pick condition
+  printS(i + 1, ans, s, sum, arr, n);
+
+  s -= arr[i];
+  ans.pop_back();
+  // Not pick Condition
+  printS(i + 1, ans, s, sum, arr, n);
+}
+
+int main()
+{
+  int arr[] = {1, 2, 1};
+  int n = 3;
+  int sum = 2;
+  std::vector<int> ans;
+  printS(0, ans, 0, sum, arr, n);
+}
