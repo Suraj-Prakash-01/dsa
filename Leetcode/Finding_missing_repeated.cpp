7305d2e3a910472f37b4372bddc15137f7ae300f
Coder <surajprakash1949@gmail.com>
2024-01-18 19:51:25 +0530

    Finding missing and repeating values

---
+++ Leetcode/Finding_missing_repeated.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+  vector<int> findMissingAndRepeatedValues(vector<vector<int>> &grid)
+  {
+    int n = grid.size(), miss, rep;
+    // Creating a vector to check a number's visit
+    vector<int> ans(n * n, 1);
+
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        // To mark a number's visit
+        ans[grid[i][j] - 1]--;
+      }
+    }
+
+    for (int i = 0; i < n * n; i++)
+    {
+      // If a number visited twice, it will have -1 in vector
+      if (ans[i] == -1)
+        rep = i + 1;
+      // If a number wasn't visited, it will have 1 in vector
+      if (ans[i] == 1)
+        miss = i + 1;
+    }
+
+    return {rep, miss};
+  }
+};
