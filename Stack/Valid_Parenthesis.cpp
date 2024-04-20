72901277436fead26aa21d120a8f946089e1ccd5
Coder <surajprakash1949@gmail.com>
2024-04-20 15:55:03 +0530

    Revised Valid Parenthesis

--- Stack/Valid_Parenthesis.cpp
+++ Stack/Valid_Parenthesis.cpp
@@ -1,3 +1,5 @@
+/* ---------- Approach 1 ----------*/
+
 bool isValidParenthesis(string s)
 {
 
@@ -41,3 +43,42 @@ bool isValidParenthesis(string s)
   else
     false; // And if there is, it's invalid
 }
+
+/* ---------- Approach 2 ---------- */
+
+class Solution
+{
+public:
+  bool isValid(string s)
+  {
+
+    // Return false if size of string is odd i.e. one bracket doesn't have matching bracket
+    if (s.size() & 1)
+      return false;
+
+    stack<char> a;
+
+    for (char c : s)
+    {
+      // If opening bracket found, push it into stack
+      if (c == '(' || c == '{' || c == '[')
+        a.push(c);
+
+      // If not, check stack top and then pop
+      else
+      {
+        // It doesn't have matching opening brakcet for closing bracket
+        if (a.empty())
+          return false;
+
+        // If everything is fine
+        else if ((c == ')' && a.top() == '(') || (c == '}' && a.top() == '{') || (c == ']' && a.top() == '['))
+          a.pop();
+        else
+          return false;
+      }
+    }
+    // If parenthesis pattern is valid, stack will be empty at the end
+    return a.empty();
+  }
+};
