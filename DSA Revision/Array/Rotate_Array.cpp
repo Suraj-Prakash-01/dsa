6dc16220ff0911c452e65f44949b9413423f4771
Suraj Prakash <surajprakash1949@gmail.com>
2025-02-26 11:07:25 +0530

    Rotate Array - 189

---
+++ DSA Revision/Array/Rotate_Array.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+  void rotate(vector<int> &nums, int k)
+  {
+    int n = nums.size();
+    k %= n;
+
+    reverse(nums.begin(), nums.end());       // Reversing the whole thing
+    reverse(nums.begin(), nums.begin() + k); // Reversing the first k elements
+    reverse(nums.begin() + k, nums.end());   // Reversing the last k elements
+  }
+};
