1553d04fb280eae158b4ec575157edce19bec9ff
Suraj Prakash <surajprakash1949@gmail.com>
2025-03-05 10:05:02 +0530

    Another solution - 2579

--- DSA Revision/Array/Count_Total_no_of_colored_cells.cpp
+++ DSA Revision/Array/Count_Total_no_of_colored_cells.cpp
@@ -10,3 +10,17 @@ public:
     return (n - 1) * 4 + coloredCells(n - 1);
   }
 };
+
+// consider 2 triangle-like areas (orange+yellow, yellow+blue)
+// 1 of both is (2n+1+1)*n/2 (Arithmetic progression)
+// their intersection is (2n+1) (yellow)
+// so ans=2*(n+1)n-(2n+1)=2*n*(n-1)+1
+
+class Solution
+{
+public:
+  long long coloredCells(int n)
+  {
+    return 2LL * n * (n - 1) + 1;
+  }
+};
